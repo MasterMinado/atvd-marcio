cad79f1a4ae0b81c28d4e45984a65921cabc8a6d
gugafreitas621 <gugafreitas621@gmail.com>
2025-05-28 01:43:58 +0000

    Finalização OK!

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Exeplo1 } from "./componentes/Exemplo1";
+import { Exemplo2 } from "./componentes/Exemplo2";
+import { Header } from "./componentes/Header";
+
+export function App(){
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Exemplo1 />
+        <Exemplo2 />
+      </main>
+    </>
+  )
+}
---
+++ src/components/Exemplo1.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+
+export function Exeplo1() {
+    const [numero, setNumero] = useState(0)
+    
+
+    useEffect(() => {
+        console.log(`o estado número mudou. seu valor agora é ${numero}`)
+    }, [numero])
+
+    return (
+        <section>
+            <h2>Exemplo 1: utilizações do useEffect</h2>
+            <p>o useEffect tem duas utilidades principais:</p>
+            <ol>
+                <li>Executar alguma função ou algum bloco de código assim que a página é carregada</li>
+                <li>monitorar a mudança de um estado para executar algum bloco de código ou função</li>
+            </ol>
+
+            <div>
+                <h3>número: {numero}</h3>
+                <button onClick={() => setNumero(numero + 1)}>Aumentar</button>
+            </div>
+        </section>
+    )
+}
